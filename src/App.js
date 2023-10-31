77f78f559df35ff415e30aa0e4a9bfb1f3e2fd7f
Soe Wunna Htun <soewunnahtun176811@gmail.com>
2023-10-31 13:59:27 +0630

    .

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import React from "react";
 import Main from "./components/Main/Main";
+import { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FilteredProducts from "./components/FilteredProduct/FilteredProducts";
 import SingleProduct from "./components/FilteredProduct/SingleProduct";
@@ -9,7 +10,27 @@ import LoginForm from "./components/Auth/LoginForm";
 import ScrollTop from "./ScrollTop";
 
 const App = () => {
+  const [isLoading, setLoading] = useState(true);
   const user = useSelector((state) => state.authSlice.user);
+
+  function someRequest() {
+    //Simulates a request; makes a "promise" that'll run for 2.5 seconds
+    return new Promise((resolve) => setTimeout(() => resolve(), 2500));
+  }
+
+  useEffect(() => {
+    someRequest().then(() => {
+      const loaderElement = document.querySelector(".loader-container");
+      if (loaderElement) {
+        loaderElement.remove();
+        setLoading(!isLoading);
+      }
+    });
+  });
+
+  if (isLoading) {
+    return null;
+  }
 
   return (
     <div className="App">
